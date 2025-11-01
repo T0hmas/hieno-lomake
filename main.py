8dea85f9f1da13e20e1d2082e64aebd663b28c13
Tomas Chen <Tomas.Chen@edu.mayk.fi>
2025-11-01 13:04:48 +0200

    uusi

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from wsgiref.simple_server import make_server
+
 
 app = Flask(__name__)
 
@@ -9,4 +11,8 @@ def root():
 @app.route("/vastaus")
 def vastaus():
     return render_template('vastaus.html', nimi=request.args['nimi'])
+
+if __name__ == '__main__':
+    with make_server("localhost", 8000, app) as server: 
+        server.serve_forever() 
 
